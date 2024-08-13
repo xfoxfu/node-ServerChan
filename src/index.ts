4100a7e6249998e6cd02576eba98a19517c1dfef
Yuze Fu <i@xfox.me>
2024-08-14 00:20:35 +0900

    ci: use github actions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 class ServerChanClient {
   private _sckey: string;
+
   constructor(sckey?: string) {
-    if (sckey) {
-      this._sckey = sckey;
-    } else if (process.env.SERVERCHAN_SCKEY) {
-      this._sckey = process.env.SERVERCHAN_SCKEY;
-    } else {
+    this._sckey = sckey ?? process.env.SERVERCHAN_SCKEY ?? "";
+
+    if (!this._sckey) {
       throw new Error("SCKEY not found!");
     }
   }
+
   async sendMessage(title: string, content?: string) {
     const url = new URL(`https://sctapi.ftqq.com/${this._sckey}.send`);
     url.searchParams.append("text", title);
@@ -18,6 +18,7 @@ class ServerChanClient {
     let result = await fetch(url.toString()).then((res) => res.json());
     if (result.errno !== 0) throw new Error(result.errmsg);
   }
+
   get sckey() {
     return this._sckey;
   }
--- src/test.ts
+++ src/test.ts
@@ -1,56 +1,67 @@
 "use strict";
 
 import ServerChan from "./";
-import chai = require("chai");
-const expect = chai.expect;
+import test from "ava";
 // store the original sckey
 let processKey = process.env.SERVERCHAN_SCKEY;
-import nock = require("nock");
 
-describe("constructor", () => {
-  it("should load parameter", () => {
-    let sc = new ServerChan("SCKEYOWN1");
-    expect(sc.sckey).to.eql("SCKEYOWN1");
-  });
-  it("should throw on no set", () => {
-    expect(() => {
-      delete process.env.SERVERCHAN_SCKEY;
-      let sc = new ServerChan();
-    }).to.throw();
-  });
-  it("should load env", () => {
-    process.env.SERVERCHAN_SCKEY = "SCKEYOWN2";
+import { http, HttpResponse } from "msw";
+export const handlers = [
+  http.get("https://sctapi.ftqq.com/:token.send", ({ params }) => {
+    if (params.token === "__SUCCESS") {
+      return HttpResponse.json({
+        errno: 0,
+        errmsg: "success",
+        dataset: "done",
+      });
+    } else {
+      return HttpResponse.json({
+        errno: 1024,
+        errmsg: "不要重复发送同样的内容",
+      });
+    }
+  }),
+];
+import { setupServer } from "msw/node";
+export const server = setupServer(...handlers);
+server.listen();
+
+test("constructor - should load parameter", (t) => {
+  let sc = new ServerChan("SCKEYOWN1");
+  t.deepEqual(sc.sckey, "SCKEYOWN1");
+});
+
+test("constructor - should throw on no set", (t) => {
+  t.throws(() => {
+    delete process.env.SERVERCHAN_SCKEY;
     let sc = new ServerChan();
-    expect(sc.sckey).to.eql("SCKEYOWN2");
   });
 });
-describe("#sendMessage", () => {
-  it("(title)", async () => {
-    nock("https://sctapi.ftqq.com")
-      .get(/[A-Za-z0-9]*\.send/)
-      .reply(200, { errno: 0, errmsg: "success", dataset: "done" });
-    process.env.SERVERCHAN_SCKEY = processKey;
-    let sc = new ServerChan();
-    await sc.sendMessage("build test " + Date.now().toString());
-  });
-  it("(title, content)", async () => {
-    nock("https://sctapi.ftqq.com")
-      .get(/[A-Za-z0-9]*\.send/)
-      .reply(200, { errno: 0, errmsg: "success", dataset: "done" });
-    process.env.SERVERCHAN_SCKEY = processKey;
-    let sc = new ServerChan();
-    await sc.sendMessage("build test " + Date.now().toString(), "test");
-  });
-  it("(title, content?) error should throw", async () => {
-    nock("https://sctapi.ftqq.com")
-      .get(/[A-Za-z0-9]*\.send/)
-      .reply(200, { errno: 1024, errmsg: "不要重复发送同样的内容" });
-    let sc = new ServerChan();
-    try {
-      await sc.sendMessage("");
-      throw new Error("No error thrown.");
-    } catch (err) {
-      expect(err?.["message"]).to.eql("不要重复发送同样的内容");
-    }
-  });
+
+test("constructor - should load env", (t) => {
+  process.env.SERVERCHAN_SCKEY = "SCKEYOWN2";
+  let sc = new ServerChan();
+  t.deepEqual(sc.sckey, "SCKEYOWN2");
+});
+
+test("#sendMessage (title)", async (t) => {
+  let sc = new ServerChan("__SUCCESS");
+  await sc.sendMessage("build test " + Date.now().toString());
+  t.pass();
+});
+
+test("#sendMessage (title, content)", async (t) => {
+  let sc = new ServerChan("__SUCCESS");
+  await sc.sendMessage("build test " + Date.now().toString(), "test");
+  t.pass();
+});
+
+test("#sendMessage (title, content?) error should throw", async (t) => {
+  let sc = new ServerChan("__FAIL");
+  try {
+    await sc.sendMessage("");
+    throw new Error("No error thrown.");
+  } catch (err) {
+    t.deepEqual(err?.["message"], "不要重复发送同样的内容");
+  }
 });
